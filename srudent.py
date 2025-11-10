e9b69cf1b5f29406c85867efad7fe606d76b0750
shrushtir2006 <shrushtiryavanki@gmail.com>
2025-11-10 11:45:35 +0530

    Create srudent.py

---
+++ srudent.py
@@ -0,0 +1,17 @@
+import sys
+       #check if correct number of arguments
+if len(sys.argv) ==3:
+  script_name = sys.argv[0]
+  name = sys.argv[1]
+  rollno = sys.argv[2]
+  print("User provided input values:")
+else:
+  script_name = sys.argv[0]
+  name="shrushti"
+  rollno="063"
+  print("no input given-using default values:")
+
+print("Script Name:",script_name)
+print("Student Name:",name)
+print("Roll Number:",rollno)
+  
